aa6f42c34b3952760432296e96ff7e8d412ff402
RioAraki <yueee.li@mail.utoronto.ca>
2016-06-09 00:37:37 -0400

    Upload eveything

---
+++ js/dropdown.js
@@ -0,0 +1,24 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function timeFunction() {
+    document.getElementById("timeDropdown").classList.toggle("show");
+}
+
+function topicFunction() {
+    document.getElementById("topicDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
